a8f4de33e7cb0e7f6d079ce6e7c4ccc0a50bf8cf
Poonam Baghel <69383331+poonambaghel@users.noreply.github.com>
2020-09-02 23:30:09 +0530

    Add files via upload

---
+++ eureka-for-proj2/src/test/java/com/udacity/eurekaforproj2/EurekaForProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.eurekaforproj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaForProj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
