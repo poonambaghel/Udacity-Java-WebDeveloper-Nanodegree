cb935891dce3b62181f3ba5719442591550725a2
Poonam Baghel <69383331+poonambaghel@users.noreply.github.com>
2020-09-16 23:14:43 +0530

    Add files via upload

---
+++ BoogleMaps-Proj2/src/main/java/com/udacity/BoogleMapsProj2/BoogleMapsProj2Application.java
@@ -0,0 +1,13 @@
+package com.udacity.BoogleMapsProj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoogleMapsProj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoogleMapsProj2Application.class, args);
+	}
+
+}
---
+++ BoogleMaps-Proj2/src/test/java/com/udacity/BoogleMapsProj2/BoogleMapsProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.BoogleMapsProj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoogleMapsProj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
